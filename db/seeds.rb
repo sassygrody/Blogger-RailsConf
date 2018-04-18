738c66bcfacee6fd62fc5b81f62e5fbb5e69b9a4
Sasha Grodzins <sasha.grodzins@devmynd.com>
2018-04-18 13:34:00 -0500

    add Post model

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Post.create([
+  {title: "2 Ways To Do Anything", content: "Do it this way and that way."},
+  {title: "Tips for getting to pet every dog you pass on the street", content: "As soon as you're in eyesight of the human, show excitment so they know they're gonna need to stop for you."}
+])
--- tutorial.md
+++ tutorial.md
@@ -47,3 +47,6 @@ Post.create([
   {title: "Tips for getting to pet every dog you pass on the street", content: "As soon as you're in eyesight of the human, show excitment so they know they're gonna need to stop for you."}
 ])
 ```
+- `rails db:seed`
+
+## Create first GraphQL Type
